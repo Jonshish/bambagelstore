0a44d621a23074f9a636ac3d3dfa6430b23f24ea
Jon Sicherman <jonshish@gmail.com>
2021-04-11 11:27:46 -0400

    meta image fix

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
-const Meta = ({ title, description, keywords, image }) => {
+const Meta = ({ title, description, keywords, image, metaImage }) => {
   return (
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="keyword" content={keywords} />
       <meta name="image" content={image} />
+      <meta property="og:image" content={metaImage} />
     </Helmet>
   )
 }
@@ -17,6 +18,7 @@ Meta.defaultProps = {
   description: 'Upstate New York boutique bagel shop!',
   keywords: 'bagels, baking, buy bagels, make bagel',
   image: '/frontend/public/images/bagelwork.jpg',
+  metaImage: '/frontend/public/images/bagelwork.jpg',
 }
 
 export default Meta
